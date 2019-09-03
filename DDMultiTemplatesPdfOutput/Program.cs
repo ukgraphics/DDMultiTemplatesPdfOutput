ae79440da5de20e0d1ad3c4b7cd4adab45e07a41
Kuni <kuni.ujiie@grapecity.com>
2019-09-03 15:57:50 +0900

    プロジェクト ファイルを追加します。

---
+++ DDMultiTemplatesPdfOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GrapeCity.Documents.Excel;
+
+namespace DDMultiTemplatesPdfOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // 新しいワークブックを生成
+            var workbook = new Workbook();
+
+            // Excelファイルをインポート
+            workbook.Open("diodocs-excel-templates.xlsx");
+
+            workbook.Save("diodocs-excel-templates.pdf", SaveFileFormat.Pdf);
+        }
+    }
+}
